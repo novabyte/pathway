995daa48d33b174158541b97777802c299f25947
Chris Molozian <chris@cmoz.me>
2014-10-27 19:06:59 +0000

    Add exception-based functions for the public API.

--- lib/pathway.ex
+++ lib/pathway.ex
@@ -5,9 +5,23 @@ defmodule Pathway do
     |> Pathway.Client.parse_req("/v1/alias")
   end
 
+  def alias!(distinct_id, alias) do
+    case Pathway.alias(distinct_id, alias) do
+      {:ok, result} -> result
+      {_, ex} -> raise ex["message"]
+    end
+  end
+
   def annotate(event, properties \\ %{}, channel \\ nil) do
     %{event: event, properties: properties, channel: channel}
     |> Pathway.Client.parse_req("/v1/annotate")
+  end
+
+  def annotate!(event, properties \\ %{}, channel \\ nil) do
+    case annotate(event, properties, channel) do
+      {:ok, result} -> result
+      {_, ex} -> raise ex["message"]
+    end
   end
 
   def identify(distinct_id, properties \\ %{}) do
@@ -15,12 +29,33 @@ defmodule Pathway do
     |> Pathway.Client.parse_req("/v1/identify")
   end
 
+  def identify!(distinct_id, properties \\ %{}) do
+    case identify(distinct_id, properties) do
+      {:ok, result} -> result
+      {_, ex} -> raise ex["message"]
+    end
+  end
+
   def page_view(distinct_id, title, url), do: track(distinct_id, "Page view", %{title: title, url: url})
+
+  def page_view!(distinct_id, title, url) do
+    case page_view(distinct_id, title, url) do
+      {:ok, result} -> result
+      {_, ex} -> raise ex["message"]
+    end
+  end
 
   def track(distinct_id, event, properties \\ %{}, channel \\ "web_site", company_id \\ nil) do
     %{distinct_id: distinct_id, event: event, properties: properties,
       company_id: company_id, channel: channel}
     |> Pathway.Client.parse_req("/v1/track")
+  end
+
+  def track!(distinct_id, event, properties \\ %{}, channel \\ "web_site", company_id \\ nil) do
+    case track(distinct_id, event, properties, channel, company_id) do
+      {:ok, result} -> result
+      {_, ex} -> raise ex["message"]
+    end
   end
 
 end
